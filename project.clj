4c805c697a8ffaddb11219605821ec9f0135ff5e
Gary Deer <gdeer81@gmail.com>
2013-12-13 22:06:40 -0600

    updated project file

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject tube-defender "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "London Undergound themed game for the 2013 GameJam"
+  :url "https://github.com/LeonKni/tube-defender"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                  [quil "1.6.0"]
+                   [simplecs "0.1.0"]]
   :main tube-defender.core)
