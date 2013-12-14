7914ed216c0819fb6abeec321c7ebec2d5cdd560
Pance <pbarnett888@gmail.com>
2013-12-13 21:37:49 -0600

    initial commit lein skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tube-defender
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ src/tube_defender/core.clj
@@ -0,0 +1,7 @@
+(ns tube-defender.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
