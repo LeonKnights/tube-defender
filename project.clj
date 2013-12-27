1aa9c0da494b2a001b78e9b0bdaa6ae2c7795e92
Gary D <gdeer81@gmail.com>
2013-12-27 16:01:33 -0600

    updating simplecs library version

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [quil "1.6.0"]
-                 [simplecs "0.1.0"]]
+                 [simplecs "0.2.0-SNAPSHOT"]]
   :main tube-defender.core)
